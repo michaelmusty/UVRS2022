f537a2326d02127cf952b7c0a3a2ecf564acd20f
michaelmusty <michaelmusty@gmail.com>
2022-06-27 21:16:57 -0400

    add 7m distance to list of allowable distances

--- utils/race.py
+++ utils/race.py
@@ -59,7 +59,7 @@ class Race:
             filename = data_path.split("/")[-1]
             a, b = filename.split(".")
             assert b == "csv"
-            assert a in ["5k", "10k", "12k"]
+            assert a in ["5k", "10k", "12k", "7m"]
             distances.append(a)
         return distances
 
